5294ed7dac02f6fab2a9d6fc52f8e49aede5ce68
mhgarry <mhgarry92@gmail.com>
2025-03-16 21:22:34 -0400

    add phrases

--- magic8/magic8.py
+++ magic8/magic8.py
@@ -6,6 +6,7 @@ question = "Will this program be awesome?"
 
 answer = ""
 
+phrases = ["Yes - definitely", "It is decidedly so", "Without a doubt", "Reply hazy, try again", "Ask again later", "Better not tell you now", "My sources say no", "Outlook not so good", "Very doubtful"]
 random_number = random.randint(1, 9)
 
 print(random_number)
